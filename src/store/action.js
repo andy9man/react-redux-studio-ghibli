4770f22a2caf45b5675f274fb968a74271a46a79
Andrew N <aninneman@gmail.com>
2018-02-01 16:58:27 -0600

    Working api call and render of people

--- src/store/action.js
+++ src/store/action.js
@@ -30,19 +30,31 @@ export function peopleFetchData() {
         console.log("fetch date now...")
         dispatch(peopleIsLoading(true));
         axios.get(`${url}/people`)
-        .then(response => {
-            dispatch( peopleFetchDataSuccess(response.data));
-            console.log(response.data);
-        })
+            .then(response => {
+                dispatch( peopleFetchDataSuccess(response.data));
+                console.log(response.data);
+            })
+            .catch( error => {
+                if (error.response) {
+                  // The request was made and the server responded with a status code
+                  // that falls out of the range of 2xx
+                  //  console.log(error.response.data.message);
+                  //  console.log(error.response.status);
+                  //  console.log(error.response.headers);
+
+                } else if (error.request) {
+                  // The request was made but no response was received
+                  // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                  // http.ClientRequest in node.js
+                  console.log(error.request);
+                } else {
+                  // Something happened in setting up the request that triggered an Error
+                  console.log('Error', error.message);
+                }
+                dispatch(peopleHasErrored(true));
+            })
 
 
-
-            //     dispatch(peopleIsLoading(false));
-
-            //     return response;
-            // })
-            // .then((response) => response.json())
-            // .then((items) => dispatch(peopleFetchDataSuccess(items)))
-            // .catch(() => dispatch(peopleHasErrored(true)));
+        dispatch(peopleIsLoading(false));
     };
 }
