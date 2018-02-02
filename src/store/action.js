323cd144ef90e991b1f00e122651a07e63faeaa2
Andrew N <aninneman@gmail.com>
2018-02-02 09:16:50 -0600

    Better error messaging

--- src/store/action.js
+++ src/store/action.js
@@ -62,20 +62,20 @@ export function fetchData(filter, type) {
             })
             .catch( error => {
                 if (error.response) {
-                  // The request was made and the server responded with a status code
-                  // that falls out of the range of 2xx
-                  //  console.log(error.response.data.message);
-                  //  console.log(error.response.status);
-                  //  console.log(error.response.headers);
-
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    //  console.log(error.response.data.message);
+                    //  console.log(error.response.status);
+                    //  console.log(error.response.headers);
+                    console.log(`Error Response: ${error.response}`);
                 } else if (error.request) {
                   // The request was made but no response was received
                   // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                   // http.ClientRequest in node.js
-                  console.log(error.request);
+                  console.log(`Error Request: ${error.request}`);
                 } else {
                   // Something happened in setting up the request that triggered an Error
-                  console.log('Error', error.message);
+                  console.log(`General Error: ${error.message}`);
                 }
                 console.log("Error has occured in obtaining data...");
                 console.log(error);
